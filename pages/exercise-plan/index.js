0a2d5020cf5d40307b5a691bc200048d7488567d
Nishant Pandey <113779980+Nishant-Pandey-2004@users.noreply.github.com>
2024-08-12 11:33:08 +0530

    Update index.js

--- pages/exercise-plan/index.js
+++ pages/exercise-plan/index.js
@@ -2,13 +2,20 @@ import ExercisePlan from "../../components/ExercisePlan";
 import React from 'react';
 import SideNav from "../../components/sidenav";
 
-const index = () => {
+const Index = () => {
   return (
-    <div className="flex h-screen">
-      <SideNav />
-      <ExercisePlan />
+    <div className="flex h-screen overflow-hidden bg-gray-50">
+      {/* Sidenav */}
+      <div className="w-1/4">
+        <SideNav />
+      </div>
+
+      {/* Main Content */}
+      <div className="flex-1 overflow-y-scroll bg-gray-50">
+        <ExercisePlan />
+      </div>
     </div>
   )
 }
 
-export default index
+export default Index;
