0b084da2435adc6be335af099e0db0e97d240d1e
Nishant <nishantpandey2004@gmail.com>
2025-04-23 14:36:29 +0530

    Vercel: Web Analytics and Speed Insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import "@/styles/globals.css";
-import 'regenerator-runtime/runtime'
+import 'regenerator-runtime/runtime';
 import '../components/Calendar.css';
 
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default MyApp;
